8a679888e8c0e10dd166eab4e62cd6ea4d5ac252
ziyouzy <603708556@qq.com>
2021-01-15 20:17:58 -0800

    创建gitee与github副本前的第一commit，新的开始

--- testfor.go
+++ testfor.go
@@ -1,96 +1,46 @@
-package main
+package main 
 
-import(
-	"time"
+import (
 	"fmt"
-	"errors"
-	"reflect"
+	//"errors"
 )
 
-func main(){
-	// for {
-	// 	fmt.Println("test start:")
-	// 	for i :=0; i<=10; i++{	
-	// 		if i == 10 { i=0 }
-	// 		fmt.Println("test i:",i)
-	// 		time.Sleep(2 * time.Second)
-	// 		continue
-	// 	}
-	// }
+type S struct{
+	a int
+	b int
+}
 
-	// j :=10
-	// go func(){
-	// 	for i := 0; i<=j; i++{
-	// 		if i == j { i=0 }
-	// 		fmt.Println("test i:",i)
-	// 		time.Sleep(1*time.Second)		
-	// 	}
-	// }()
+type newS S
 
-	// time.Sleep(15*time.Second)
-	// j=20
-	// i :=8
-	// switch i{
-	// case 1:
-	// 	fmt.Println("i is 1")
-	// case 2:
-	// 	fmt.Println("i is 2")
-	// case 8:
-	// 	fmt.Println("i is 8")
-	// default:
-	// 	fmt.Println("i is ?")
+var (
+	x int
+	Y int
+	s S
+	NewS newS
+)
 
-	// }
+var z int =1
+var Z int
+//Z=3
 
-	// select 8{
-	// case i>3:
-	// 	fmt.Println("i > 1")
-	// case i<9:
-	// 	fmt.Println("i > 2")
-	// case i==8:
-	// 	fmt.Println("i > 8")
-	// default:
-	// 	fmt.Println("i > ?")
-
-	// }
-
-	type testF func(int,int)error
-
-	// n :=199
-	// var testf1 =func(a int, b int)error{
-
-	// 	if n ==199 {n =299;    return errors.New("n 是199")}
-	// 	if n !=199 {return errors.New("n 不是199")}
-
-	// 	if a ==0||b==0 {return errors.New("a与b都不能为0")}
-	// 	fmt.Println("a:",a,"b:",b)
-	// 	return nil
-	// }(0,0)
-	// fmt.Println("type:", reflect.TypeOf(testf1), "value:", testf1)
+//zz :=123
 
 
-	m :=199
-	var testf2 =func(a int, b int)error{
+var s2 S =S{a :1,b:2}
 
-		if m ==199 {m =299;    return errors.New("m 是199")}
-		if m !=199 {return errors.New(fmt.Sprintf("%s%d", "m 不是199,而是", m))}
+func (p newS)test(){
+	fmt.Println("mytest")
+}
 
-		if a ==0||b==0 {return errors.New("a与b都不能为0")}
-		fmt.Println("a:",a,"b:",b)
+type I interface{
+	test()
+}
 
-		return nil
-	}
 
-	fmt.Println("type:", reflect.TypeOf(testf2), "value:", testf2(1,2))
-	time.Sleep(5*time.Second)
-	fmt.Println("type:", reflect.TypeOf(testf2), "value:", testf2(1,2))
-	fmt.Println("外层m:",m)
+func main() {
+	var aa,bb,cc string; aa,bb,cc="aa","bb","cc"
+	var ZX int =2
+	fmt.Println(ZX,z,Z)
 
-	// testf2 :=func(a int, b int){
-	// 	fmt.Println("a:",a,"b:",b)
-	// }(2,3)
-	//_ =testf2
-	//fmt.Println(testf2)
-
-	select{}
+	fmt.Println(s2,aa,bb,cc)
 }
--- viperbuilder/singleviper.go
+++ viperbuilder/singleviper.go
@@ -56,8 +56,29 @@ type SingleViper struct{
 }
 
 func (p *SingleViper)watching() {
-	p.V.WatchConfig()
+	/** 不能直接去p.V.OnConfigChange = func(e fsnotify.Event) { fmt.Println("Config file changed:", e.Name)}
+	  * 因为V的内部根本不存在 OnConfigChange变量
+	  * 只存在私有的onConfigChange变量，以及OnConfigChange方法
+	  * 因此大神的设计模式是这样的：
+	  * 私有变量通过名称相同，但首字母大写的公有方法实现初始化
+	  * 同时为了匹配单例模式，单例函数(非方法)与viper结构体的OnConfigChange方法名称一致，我一直也在用这种设计模式
+
+	  * 大神这种设计模式的目的只有一个，就是给使用者暴露一个自定义函数
+	  * 为了实现这个目的这个函数必须定义在结构体内部，而不能把他设计成这个结构体的方法
+	  * 同时这也是借助了闭包的特性实现的，因为当使用者具象话这个函数的具体功能后
+	  * 该函数就会以函数变量的形式存在于结构体内部了，也就是一个内部字段
+	  * 等同于在一个方法内部使用了闭包
+	  * 或者说，一个方法内部的闭包函数对象，都可以把转移成结构体自身的内部字段
+	  * 这样会变得十分优雅，也是函数变量的意义与价值所在
+	  */
+
 	p.V.OnConfigChange(func(e fsnotify.Event) {
+			  
+	    /** OnConfigChange方法虽然没有在变量名上表达出Set、Init等初始化意图
+	      * 但是其的功能只有一个，那就是初始化viper.onConfigChange这一私有字段
+	      * 以后我也这么写就好，变量名不用加Set、Init之类的
+		  */
+		  
 		fmt.Println("Config file changed:", e.Name)
 
 		err := p.V.ReadInConfig() // 搜索路径，并读取配置数据
@@ -71,6 +92,8 @@ func (p *SingleViper)watching() {
 			return
 		}
 	})
+
+p.V.WatchConfig()
 }
 
 func (p *SingleViper)Destory(){
